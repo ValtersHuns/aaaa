f431eeb99031cd79dd03fd98a7a18a7bb6dd576b
ValtersHuns <60145218+ValtersHuns@users.noreply.github.com>
2020-03-09 18:44:44 +0200

    a
    a

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ from datetime import datetime
 
 import chats
 
+
 app = Flask('app')
 
-ENV = 'dev'
+ENV = 'prod'
 
 if ENV == 'dev':
-  app.debug = True
-  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:valtersh26/postgres'
-
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:valtersh26@localhost/postgres'
 else:
-  app.debug = False
-  app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qxjnzonbylcpsu:5345f4eaa942791f5b2b718eef3414b1b58177637e3b52c1b205b5ed876c0c0d@ec2-54-195-247-108.eu-west-1.compute.amazonaws.com:5432/d5qem9lmnv7j9b'
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
